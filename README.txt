14aa559e6bfb5b93fd2ffd88ce22445d7de4d506
sebasgverde <sgarci18@eafit.edu.co>
2013-10-31 20:14:48 -0500

    pruba con recorridos

--- README.txt
+++ README.txt
@@ -3,7 +3,10 @@ practica organizacion de computadores
 nasm with c library
 
 http://stackoverflow.com/questions/8523905/calling-c-io-functions-in-linux-nasm-assembly
+
 http://forum.nasm.us/index.php?topic=1532.0
 
 gdb
 http://www.csee.umbc.edu/portal/help/nasm/nasm.shtml
+
+http://myweb.lmu.edu/dondi/share/sp/nasm64.pdf
--- practicaOrg.asm
+++ practicaOrg.asm
@@ -4,15 +4,42 @@
  extern printf
 
 segment .data
-        msgBienv:       db      'Bienvenido a la practica de organizacion de computadores', 0AH
+        msgBienv:	db	'Bienvenido a la practica de organizacion de computadores', 0AH
+        arreglo:	dd	12,32,34,23,4,234,34,5,345,34,5,34,5
+        formatoDec: db '%d', 0AH,0
+		format:		db '%d', 0AH,0
+
+
+segment .bss
+        temp:	resq 1
 
  segment .text
 
 _start: 
 mov rdi, msgBienv ; rdi gets the first argument (a pointer)
-mov rax, 0 
-call printf
-inter:
+	mov rax, 0 
+	call printf
+	
+
+	mov	eax, 0
+	mov	ebx, 5
+		ciclo1:
+	push rax
+	push rbx
+	cmp	eax,ebx	
+	jge finciclo1
+		mov dword rsi, rax
+		mov rdi, formatoDec 
+		mov eax, 0
+			inter:
+		call printf
+		pop rbx
+		pop rax
+		inc  rax
+	jmp ciclo1
+	finciclo1:
+
+
 jmp Exit
 
 
@@ -20,3 +47,7 @@ jmp Exit
         mov     eax, sys_Exit
         mov     ebx, 0
         int     80H
+
+
+
+
