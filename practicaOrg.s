14bbcc16a27814c84f2057f02640371466834ae1
sebasgverde <sgarci18@eafit.edu.co>
2013-11-23 21:30:33 -0500

    todo listo pero con ua corrupcion toda rara ahi para llamar a la de c

--- practicaOrg.s
+++ practicaOrg.s
@@ -11,22 +11,23 @@
  extern dibujar
  extern dividir
  extern imprimirBit
+ extern scanf
 
 segment .data
-        msgBienv:	db	'Bienvenido a la practica de organizacion de computadores', 0AH,0
+        msgBienv:	db	'Bienvenido a la practica de organizacion de computadores, ingrese el comando de la operacion deseada', 0AH, ' 1: espejo a lado izquierdo', 0AH, ' 2: espejo a lado derecho', 0AH, ' 3: inversion total de la imagen', 0AH, ' 4: inversion de color', 0AH,0
         arreglo:	dd	185113,32,34,23,4,234,34,5,345,34,5,34,5
         tamArreglo: dd 	52;13*4
-        formatoDec: db 'Processing a %dx%d image with %d channels with step %d', 0AH,0
+        formatoDec: db 'Processing in asm a %dx%d image with %d channels with step %d', 0AH,0
         formatoDec2: db '%d', 0AH,0
         formatoString: db '%s', 0AH,0
         formatoByte: db '%b',0AH,0
+        formatoComando: db '%d',0 
         otroFormato: db 'i %d j %d k %d',0AH,0
         archivoNombre: db 'salida.txt',0AH,0;para intentar poner el output en un archivo con fprintf
         dos: dd 2
 
 
 segment .bss
-        temp:	resq 1
         imagen1: resq 1
         imagen2: resq 1
         imagenStruct: resq 1
@@ -39,13 +40,22 @@ segment .bss
         i:  resd  1
         j:  resd  1
         k:  resd  1
+        temp: resd 1
         pos1: resq  1
         pos2: resq  1
+        comando: resd 1
 
 
  segment .text
 
 _start: 
+
+mov rdi, msgBienv
+call  printf
+
+mov rdi, formatoComando
+mov rsi, comando
+call scanf
 
 mov rbx, [rsp + 16];rsp argc, + 8 path, + 16 arg1 .... 
 mov [imagen1], rbx
@@ -101,25 +111,15 @@ mov rsi, [height]
 mov rdx, [width]
 mov rcx, [channels]
 mov r8, [step]
-
-
 call printf
+
+
 
 mov rdi, formatoDec2
 mov rsi, [data]
           
 
 call printf
-
-
-mov rdi, [height]
-mov rsi, [width]
-mov rdx, [channels]
-mov rcx, [step]
-mov r8, [data]
-mov rdi , [imagenStruct]
-call invertir
-
 
 
 mov dword [i],0
@@ -144,17 +144,13 @@ jge fincicloi
       mov rsi, [i]
       mov rdx, [j]
       mov rcx, [k]
-      call printf
+      ;call printf
 
       ;---cambio-----
 
       mov rax, [data]
-      ;mov [pos1], rax
       mov qword [pos1], 0
-      mov [pos2], rax
-
-
-
+      mov qword [pos2], 0
 
       mov edx,0
       mov eax, [i]
@@ -162,12 +158,7 @@ jge fincicloi
       add eax, [k]
 
       add [pos1], eax
-      ;add [pos2], rdi
-
-
-
-
-
+      add [pos2], eax
 
       mov edx,0
       mov eax, [j]
@@ -175,24 +166,49 @@ jge fincicloi
       mov edi, eax
 
       add [pos1], edi
+
+      mov eax,[step]
+      sub eax, edi
+
+      add [pos2], eax
+
       mov rax, [data]
       add [pos1], rax
+      add [pos2], rax
+  
 
+        ;----------casos-----------
 
-      mov rdi,[pos1]
-      call imprimirBit
+        mov eax, [comando]
+        mov ebx, 1
+        cmp eax,ebx
+        je izq
+          mov eax, [comando]
+          mov ebx, 2
+          cmp eax,ebx
+          je der
+            je izq
+            mov eax, [comando]
+            mov ebx, 3
+            cmp eax,ebx
+            je inv
+              je izq
+              mov eax, [comando]
+              mov ebx, 4
+              cmp eax,ebx
+              je col
+              jmp fincicloi              
 
-      ;mov rax, [pos1]
-      ;mov byte [rax],0
+        izq: call espejoAIzq
+        jmp finCasos
+        der: call espejoADer
+        jmp finCasos
+        inv: call inversionCompletaAsm
+        jmp finCasos
+        col: call invertirColor
+        jmp fincicloi
 
-      ;mov eax,[step]
-      ;sub eax, edi
-
-      ;add [pos2],eax
-
-
-
-
+        finCasos:
       ;--------------
 
       inc dword [k]
@@ -207,7 +223,6 @@ fincicloi:
 
 
 
-
 mov rdi,[imagen2]
 mov rsi,[imagenStruct]
 call dibujar
@@ -215,6 +230,66 @@ call dibujar
 
 jmp Exit
 
+
+
+espejoAIzq:
+
+    mov rdi,[pos1]
+    mov rsi,[pos2]
+    mov rdi, [rdi]
+    mov [rsi],rdi
+
+    ret
+
+espejoADer:
+
+    mov rdi,[pos1]
+    mov rsi,[pos2]
+    mov rsi, [rsi]
+    mov [rdi],rsi
+
+    ret
+
+inversionCompletaAsm:
+
+    mov rdi,[pos1]
+    mov r8, [rdi]
+
+    mov rdi,[pos1]
+    mov rsi,[pos2]
+    mov rsi,[rsi]
+    mov [rdi],rsi
+    
+    mov rsi,[pos2]
+    mov [rsi],r8
+
+    ret
+
+invertirColor:
+
+    mov rdi,[pos1]
+    mov rsi,[pos2]
+
+    mov al, 255
+    sub al, [rsi]
+    mov [rsi],al
+
+    mov al, 255
+    sub al, [rdi]
+    mov [rdi],al
+
+    ret
+
+
+inversionCompletaC:
+  mov rdi, [height]
+  mov rsi, [width]
+  mov rdx, [channels]
+  mov rcx, [step]
+  mov r8, [data]
+  mov rdi , [imagenStruct]
+  call invertir
+  ret
 
  Exit: 
         mov     eax, sys_Exit
